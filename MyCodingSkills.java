d64bf50463427deaee83556a4730c64c11e27e1d
Jon Lake <jlake21@ford.com>
2022-05-24 13:08:55 -0600

    Add Solution

---
+++ MyCodingSkills.java
@@ -0,0 +1,12 @@
+public class MyCodingSkills {
+  public static void main(String[] args) {
+    System.out.println("HTML");
+    System.out.println("CSS");
+    System.out.println("CLI");
+    System.out.println("Git");
+    System.out.println("JavaScript");
+    System.out.println("TypeScript");
+    System.out.println("Angular");
+    System.out.println("Java");
+  }
+}
