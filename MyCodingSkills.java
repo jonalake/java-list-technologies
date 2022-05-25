5bb9a10886d93271bf4d08b9dcd312cc29f29a64
Jon Lake <jlake21@ford.com>
2022-05-25 08:29:19 -0600

    Add iteration

--- MyCodingSkills.java
+++ MyCodingSkills.java
@@ -1,12 +1,8 @@
 public class MyCodingSkills {
   public static void main(String[] args) {
-    System.out.println("HTML");
-    System.out.println("CSS");
-    System.out.println("CLI");
-    System.out.println("Git");
-    System.out.println("JavaScript");
-    System.out.println("TypeScript");
-    System.out.println("Angular");
-    System.out.println("Java");
+    String[] technologies = { "HTML", "CSS", "CLI", "Git", "JavaScript", "TypeScript", "Angular", "Java" };
+    for (String technology : technologies) {
+      System.out.println(technology);
+    }
   }
 }
